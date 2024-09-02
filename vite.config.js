714cdcc31e9325f45cd22c53b8f3b7fca8fed66d
Andrey Chaykin <151469539+p1kcherino@users.noreply.github.com>
2024-09-02 09:02:18 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/to-do-scloud-react-",
   plugins: [react()],
+  base: "/to-do-scloud-react-",
 });
